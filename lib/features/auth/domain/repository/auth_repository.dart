8e792041909cdf69f07f2f8b5979e607bbcf3eee
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2025-07-10 16:26:25 +0700

    Add User and Failure entities; implement AuthRepository interface with authentication methods

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,22 @@
+import 'package:blog_app/core/common/entities/user.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:fpdart/fpdart.dart';
+
+//Định nghĩa một interface cho AuthRepository để quản lý các chức năng liên quan đến xác thực người dùng.
+abstract interface class AuthRepository {
+  //Đăng ký người dùng mới bằng email và mật khẩu.
+  Future<Either<Failure, User>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  //Đăng nhập người dùng bằng email và mật khẩu.
+  Future<Either<Failure, User>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+
+  //Lấy thông tin người dùng hiện tại.
+  Future<Either<Failure, User>> currentUser();
+}
