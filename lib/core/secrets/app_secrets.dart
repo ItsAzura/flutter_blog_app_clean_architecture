9939fad8f28ce5f05b1f719d28c730db69a64342
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2025-07-11 14:02:56 +0700

    Integrate Supabase and related plugins; update app secrets, main entry point, and dependencies

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://wvarwmtgrrkcuhogljbu.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2YXJ3bXRncnJrY3Vob2dsamJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMzYyNjEsImV4cCI6MjA2NTYxMjI2MX0.hyEGGp7mFWOJl8soRQRI6--ex_Qj2_lwLbGdIXCYsmE';
+}
