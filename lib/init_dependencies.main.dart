8a80e2c5293018ebe837cb3acb522ea538a432d2
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2025-07-11 21:57:46 +0700

    Add dependency injection setup for authentication and Supabase initialization

---
+++ lib/init_dependencies.main.dart
@@ -0,0 +1,38 @@
+part of 'init_dependencies.dart';
+
+//Tạo một đối tượng GetIt để quản lý các phụ thuộc trong ứng dụng
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  //Khởi tạo auth
+  _initAuth();
+
+  //khởi tạo Sdk Supabase
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  //Đăng ký SupabaseClient vào serviceLocator để sử dụng trong các phần khác của ứng dụng
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+void _initAuth() {
+  serviceLocator
+    //* Datasource
+    //Đăng ký class AuthRemoteDataSourceImpl là implementation cho interface AuthRemoteDataSource
+    ..registerFactory<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(serviceLocator()),
+    )
+    //* Repository
+    // đăng ký AuthRepositoryImpl làm repository xử lý logic nghiệp vụ.
+    ..registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(serviceLocator()),
+    )
+    //* Usecases
+    // Đăng ký UserSignUp use case để xử lý đăng ký người dùng
+    ..registerFactory(() => UserSignUp(serviceLocator()))
+    //* Bloc
+    // Đăng ký AuthBloc để quản lý trạng thái đăng nhập và đăng ký
+    ..registerLazySingleton(() => AuthBloc(userSignUp: serviceLocator()));
+}
