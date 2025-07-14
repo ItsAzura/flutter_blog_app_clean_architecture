abb0ddc89731ad64d3cb34efda88497ac3a001b2
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2025-07-14 14:09:40 +0700

    Add reading time calculation and enhance BlogCard widget; implement navigation and display blog topics

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,27 +1,78 @@
+import 'package:blog_app/core/theme/app_palette.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    context.read<BlogBloc>().add(BlogFetchAllBlogs());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Blog Page'),
+        title: const Text('Blog App'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.add_circle_outline),
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const AddNewBlogPage()),
               );
             },
+            icon: const Icon(Icons.add),
           ),
         ],
       ),
-      body: Center(child: Text('Welcome to the Blog Page!')),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.error),
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                padding: EdgeInsets.all(10),
+                behavior: SnackBarBehavior.floating,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is BlogsDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return BlogCard(
+                  blog: blog,
+                  color: index % 2 == 0
+                      ? AppPalette.gradient1
+                      : AppPalette.gradient2,
+                );
+              },
+            );
+          }
+          return const SizedBox();
+        },
+      ),
     );
   }
 }
